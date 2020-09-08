71eb12bf2590853a557e953ba33b42e85de9ba36
Alon Gibli <agibli@gmail.com>
2020-09-07 22:01:42 -0700

    adds expression output tests

--- tests/test_expression.py
+++ tests/test_expression.py
@@ -37,6 +37,30 @@ class TestExpression(unittest.TestCase):
         self.assertNotIn(A(1), test_set)
         self.assertNotIn(B(0), test_set)
 
+    def test_outputs(self):
+        class A(Expression): pass
+        out1_ = Output(A)
+        out2_ = Output(SelfType)
+        class B(Expression):
+            out1 = out1_
+        class C(B):
+            out2 = out2_
+        b = B()
+        c = C()
+        self.assertIsInstance(b.out1, A)
+        self.assertIsInstance(c.out1, A)
+        self.assertIsInstance(c.out2, C)
+        self.assertEqual(b.out1, B.out1(b))
+        self.assertEqual(c.out1, C.out1(c))
+        self.assertEqual(c.out2, C.out2(c))
+        self.assertEqual(out1_.name, "out1")
+        self.assertEqual(out2_.name, "out2")
+        self.assertEqual(out1_.expression_type, B.out1)
+        self.assertEqual(out1_.expression_type, C.out1)
+        self.assertEqual(out2_.expression_type, C.out2)
+        self.assertTupleEqual(B._outputs, (out1_,))
+        self.assertTupleEqual(C._outputs, (out1_, out2_))
+
 
 if __name__ == '__main__':
     unittest.main()
